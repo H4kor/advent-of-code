83eb2a72523238e7aa0ea0f2f23f23e68e2792a4
Niko Abeler <niko@rerere.org>
2022-12-01 20:32:41 +0100

    Update day_one.py

--- day_one.py
+++ day_one.py
@@ -9,3 +9,18 @@ for l in ls:
           c = 0
           continue
     c += int(l.strip())
+print(m)
+
+# second part
+
+f = open("input", "r")
+ls = f.readlines()
+c = 0
+m = []
+for l in ls:
+    if l.strip() == "":
+         m.append(c)
+         c = 0
+         continue
+    c += int(l.strip())
+print(sum(sorted(m, reverse=True)[:3]))
